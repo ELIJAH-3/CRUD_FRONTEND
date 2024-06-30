8cc58902255a27c493abcb0c8bf86a54f8935fb6
ABHIJEET K <the.abhijeet.kum@gmail.com>
2024-06-30 17:32:12 +0530

    initial commit after restart

---
+++ BACKEND/server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express();
+
+app.listen(8081, () => {
+    console.log("listening to 8081");
+    // run command "node server.js" to start the server
+})
